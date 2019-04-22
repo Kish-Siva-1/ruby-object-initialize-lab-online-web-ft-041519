1476911a3b16ef94cc1951511edda3759a1d06b1
Kish Siva <rodes88@msn.com>
2019-04-22 20:24:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,10 @@ class Dog
   attr_accessor :name, :breed
   
   def initialize(new_name, new_breed)
-   
-    new_breed = "Mutt"
-    new_name = "Mutt"
-    
     @name = new_name 
     @breed = new_breed
-    
   end
+  
+  initialize("Mutt","Mutt")
   
 end
